b4af459a532f2a96e33d5f507d63b86e21a4f875
Paolo G. Giarrusso <p.giarrusso@gmail.com>
2014-10-05 01:22:32 +0200

    Segregate code requiring UndecidableInstances

---
+++ src/Sec7UndecidableInstances.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE UnicodeSyntax #-}
+{-# LANGUAGE PatternSynonyms #-}
+
+-- | Segregate code requiring UndecidableInstances.
+module Sec7UndecidableInstances where
+
+import Syntactic
+
+-- Example from the paper
+class OptAdd sym dom where
+  optAddSym ∷ sym a → Args (AST dom) a → AST dom (Full (Result a))
+
+optAdd :: OptAdd dom dom => ASTF dom a -> AST dom (Full a)
+optAdd = query optAddSym
+
+instance (OptAdd sub1 dom, OptAdd sub2 dom) ⇒ OptAdd (sub1 :+: sub2) dom where
+  optAddSym (InjL s) = optAddSym s
+  optAddSym (InjR s) = optAddSym s
+
+optAddDefault :: (OptAdd dom dom, sym :<: dom) =>
+  sym a → Args (AST dom) a → AST dom (Full (Result a))
+optAddDefault s = appArgs (Sym (inj s)) . mapArgs optAdd
+
+instance (NUM :<: dom, OptAdd dom dom) ⇒ OptAdd NUM dom where
+  optAddSym Add (a :* NUM 0 :* Nil) = optAdd a
+  optAddSym s as = optAddDefault s as
+
+instance (Logic :<: dom, OptAdd dom dom) ⇒ OptAdd Logic dom where
+  optAddSym = optAddDefault
+
+instance (If :<: dom, OptAdd dom dom) ⇒ OptAdd If dom where
+  optAddSym If (c :* t :* e :* Nil) =
+    appArgs (Sym (inj If)) (optAdd c :* t :* e :* Nil)
--- src/Syntactic.hs
+++ src/Syntactic.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE FlexibleContexts #-}
@@ -484,31 +483,7 @@ typedFold2 :: ∀ dom c.
      (∀ a. ASTF dom a              → c (Full a))
 typedFold2 f = query $ \sym → f sym . mapArgs (typedFold2 f)
 
--- Example from the paper
-class OptAdd sym dom where
-  optAddSym ∷ sym a → Args (AST dom) a → AST dom (Full (Result a))
-
-optAdd :: OptAdd dom dom => ASTF dom a -> AST dom (Full a)
-optAdd = query optAddSym
-
-instance (OptAdd sub1 dom, OptAdd sub2 dom) ⇒ OptAdd (sub1 :+: sub2) dom where
-  optAddSym (InjL s) = optAddSym s
-  optAddSym (InjR s) = optAddSym s
-
-optAddDefault :: (OptAdd dom dom, sym :<: dom) =>
-  sym a → Args (AST dom) a → AST dom (Full (Result a))
-optAddDefault s = appArgs (Sym (inj s)) . mapArgs optAdd
-
-instance (NUM :<: dom, OptAdd dom dom) ⇒ OptAdd NUM dom where
-  optAddSym Add (a :* NUM 0 :* Nil) = optAdd a
-  optAddSym s as = optAddDefault s as
-
-instance (Logic :<: dom, OptAdd dom dom) ⇒ OptAdd Logic dom where
-  optAddSym = optAddDefault
-
-instance (If :<: dom, OptAdd dom dom) ⇒ OptAdd If dom where
-  optAddSym If (c :* t :* e :* Nil) =
-    appArgs (Sym (inj If)) (optAdd c :* t :* e :* Nil)
+-- For the examples from the paper, see Sec7UndecidableInstances.hs
 
 -- Sec. 8: Mutually recursive types
 
